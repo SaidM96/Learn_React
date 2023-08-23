a352f4beec3b6f4ef687f4798ca3942273bdf2b8
Said Mia <saiid.revolution@gmail.com>
2023-08-23 10:35:11 +0100

    first little project in reactjs

---
+++ project_01/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function change(event) {
+    const { name, value } = event.target;
+    switch (name) {
+      case "fName":
+        setContact({
+          fName: value,
+          lName: contact.lName,
+          email: contact.email
+        });
+        break;
+      case "lName":
+        setContact({
+          fName: contact.fName,
+          lName: value,
+          email: contact.email
+        });
+        break;
+      case "email":
+        setContact({
+          fName: contact.fName,
+          lName: contact.lName,
+          email: value
+        });
+        break;
+      default:
+        break;
+    }
+  }
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" placeholder="First Name" onChange={change} />
+        <input name="lName" placeholder="Last Name" onChange={change} />
+        <input name="email" placeholder="Email" onChange={change} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
